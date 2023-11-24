7eb31ecc4d1f6537a2807fdf877ef67987bbcad4
CIC-SL\rpovedano <rpovedano@cic.es>
2023-11-24 15:59:25 +0100

    Añadido gifitem y llamada a la apirest con fetch

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,27 @@
+import { useState, useEffect } from "react";
+
+import { GifItem } from "./GifItem";
+
+import { getGif } from "../helpers/getGifs";
+
 export const GifGrid = ({category}) => {
 
-    const gifs = [1,2,3,4];
+  const [images, setImages] = useState([]);
 
+  useEffect( () => {    
+    getGif(category).then(
+      newImages => setImages(newImages)
+    );
+  }, [] );
 
   return (
     <>
         <h3>{ category }</h3>
-        {gifs.map((gif) => (
-            <p>{gif}</p>
-        ))}
+        <div className="card-grid">
+          {images.map(({id, title}) => (
+            <GifItem key={id} />
+          ))}
+        </div>
     </>
   )
 }
---
+++ src/components/GifItem.jsx
@@ -0,0 +1,6 @@
+
+export const GifItem = () => {
+  return (
+    <div className="card">GifItem</div>
+  )
+}
