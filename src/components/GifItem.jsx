c50d251cc8637f25419c7cbd044d520acf1e6fba
CIC-SL\rpovedano <rpovedano@cic.es>
2023-11-30 16:55:34 +0100

    proptypes

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types';
+
+
 
 export const GifItem = ({title, url}) => {
 
@@ -7,4 +10,9 @@ export const GifItem = ({title, url}) => {
       <p>{ title }</p>
     </div>
   )
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
